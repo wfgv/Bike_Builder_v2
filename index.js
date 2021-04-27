890b8822d25acfd9cde13a628732f81531b5c5f7
wfgv <willian.voelz@gmail.com>
2021-04-27 20:41:38 +0000

    .env

--- index.js
+++ index.js
@@ -5,39 +5,22 @@ const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv'); 
 
 var app = express();
 var port = 8000;
 
+dotenv.config();
 app.use(bodyParser.json()); //express use json
 app.use(logger('tiny'));
 
 app.use(require('./routes')); // included as a middleware
 
-const dbURI = "mongodb://localhost/test";
+const dbURI = process.env.DB_URL;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology:true})
         .then((result) => console.log('connected to db'))
         .catch((err) => console.log(err));
-
-   // http.createServer((req, res)=>{
-    //  res.write(users.join("\n ")); // write a response
-      //res.write(emails.join("\n")); // write a response
-    //  res.end(); //end the response
-    //}).listen(8000); // listen for requests on port 8000
-
-//     let users = []; // names of users will be stored here
-//     //let emails = [];
-// (async function getNames(){
-//   try{
-//     const {data} = await axios.get("https://swapi.dev/api/people/");
-//     users = data.results.map(user=>user.name);
-//     //emails = data.map(email=>email.email);
-//     console.log(users)
-//   } catch(error){
-//     console.log(error)
-//   }
-// })();
 
 app.listen(port, function(err){
     console.log('Listening on port:' + port);
